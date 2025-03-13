96613e364b1495eeae50a1bbf3a517d0b53b4683
Theodor Kallekleiv Vedøy <theodor.vedoy@gmail.com>
2025-03-13 12:11:38 +0100

    Attempted to fix looping loading deno deploy

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
 // deno-lint-ignore-file no-explicit-any
 
 import express from "npm:express@4.21.1";
-
 import { parseArgs } from "jsr:@std/cli/parse-args";
-
 
 const { args } = Deno;
 const DEFAULT_PORT = 8000;
 const argPort = parseArgs(args).PORT || Number(Deno.env.get("PORT"));
 console.log("argPort", argPort);
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
+
 // Create a new app
 const app = new express();
 
@@ -27,9 +26,14 @@ app.use(express.static("dist"));
 
 // Serve static files and index.html
 
-
 // Start the server
-console.log("Server is running on http://localhost:8000");
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+
+  // Inform Deno Deploy that the server has started
+  fetch(`http://localhost:${port}`).then(() => {
+    console.log("Deno Deploy informed that the server has started");
+  }).catch((err) => {
+    console.error("Failed to inform Deno Deploy:", err);
+  });
 });
